c455c9580757fb5dbf782a006a9ff7db458690e1
nickelink <160444947+nickelink@users.noreply.github.com>
2024-09-10 20:10:31 +0300

    Добавит разметку главной страницы

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+import './MainPage.css'
+import {Header} from "../../components";
+
+export const MainPage = () => {
+  return (<div className="page-wrapper">
+    <Header />
+    <div className="form">Form</div>
+    <div className="footer">Footer</div>
+  </div>)
+}
