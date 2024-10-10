0ca8ba4cdf857b0ea18529ef81490aea26a2f605
zxclownator3000 <zxclownator3000@github.com>
2024-10-10 23:25:04 +0300

    добавила footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,23 @@
 import styles from "./Footer.module.css";
+
 export const Footer = () => {
   return (
     <div id="footer" className={styles.footer}>
-      Footer
+      <div className={styles.text}> Социальные сети</div>
+      <div className={styles.icons}>
+        <a href="https://web.telegram.org">
+          <img src="../../../public/telegram.png" alt="" />
+        </a>
+        <a href="https://www.tiktok.com">
+          <img src="../../../public/tik-tok.png" alt="" />
+        </a>
+        <a href="https://vk.com">
+          <img src="../../../public/vkontakte.png" alt="" />
+        </a>
+        <a href="https://web.whatsapp.com/">
+          <img src="../../../public/whatsapp.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 };
